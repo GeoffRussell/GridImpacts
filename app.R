e674889e87fa1f9fb0f6854c4563fe7414f94981
Geoff Russell <geoffrey.russell@gmail.com>
2024-12-26 18:05:33 +1030

    Getting github synced

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Samply Shiny App"),
+    titlePanel("Samply Shiny Application"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
